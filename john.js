f51170c29280cf3aaa13863c80540ebda007f6d6
melloyk <kmelloy98@gmail.com>
2022-06-30 22:34:59 -0400

    test voice channel update detection

--- john.js
+++ john.js
@@ -5,3 +5,24 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 const TOKEN = process.env.TOKEN;
+
+bot.login(TOKEN);
+
+bot.on('ready', () => {
+  console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('voiceStateUpdate', (oldState, newSTate) => {
+	let newUserChannel = newMember.channelID;
+       let oldUserChannel = oldMember.channelID;
+    
+       if(newUserChannel === "Channel id here") //don't remove ""
+       { 
+           // User Joins a voice channel
+           console.log("Joined vc with id "+newUserChannel);
+       }
+       else{
+           // User leaves a voice channel
+           console.log("Left vc");
+       }
+});
