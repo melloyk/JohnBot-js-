83282b5d5d6d66a92913e16c2a450e1c47f70e35
melloyk <kmelloy98@gmail.com>
2022-07-12 23:07:14 -0400

    add destroy command

--- john.js
+++ john.js
@@ -22,20 +22,17 @@ bot.on('voiceStateUpdate', (oldState, newState) => {
 	let adapterCreator = newState.guild.voiceAdapterCreator;
     let newUserChannel = newState.channelId;
     let oldUserChannel = oldState.channelId;
-	console.log(guildId + " " + newUserChannel + " " + adapterCreator);
+    let connection = null;
 
     if(newUserChannel === "883867547986952228") //don't remove ""
     { 
        // User Joins a voice channel
-       console.log("atempting to joined vc with id " + newUserChannel);
-       const connection = joinVoiceChannel({
+      connection = joinVoiceChannel({
          channelId: newUserChannel,
          guildId: guildId,
          adapterCreator: adapterCreator});
-       console.log("connection " + connection);
     } else{
        // User leaves a voice channel
-       console.log("Left vc");
-       oldUserChannel.leave();
+       connection.destroy();
     }
 });
